a177d0ac19f2d7a17314452f228011a4622be57c
Mati <m.cotti1@campus.unimib.it>
2024-02-18 00:22:49 +0100

    prova

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -23,7 +23,7 @@ function App() {
                 navItems={nav}
             >
                 <Routes>
-                    <Route path="/Matilde74.github.io" element={<Home/>}/>
+                    <Route path="/matilde74.github.io" element={<Home/>}/>
                     <Route path="/characters" element={<Characters/>}/>
                     <Route path="/characters/:number" element={<CharacterDetail/>}/>
                     <Route path="/info" element={<Info/>}/>
