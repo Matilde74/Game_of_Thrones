e8fc6d0a27a6a71c4b2bececff16121d3d72741a
Mati <m.cotti1@campus.unimib.it>
2024-02-18 14:31:19 +0100

    info

--- src/views/Info/Info.js
+++ src/views/Info/Info.js
@@ -2,9 +2,31 @@ import React from "react";
 import style from "./Info.module.css"
 function Info (){
     return (
-        <div >
+        <div>
             <div className={style.sfondo} >
-                <h2> Lavoro di Matilde Cotti per il corso di Applicazioni Web: Progettazione e Sviluppo</h2>
+                <h2>
+                    Questo progetto è stato creato attraverso l’utilizzo delle API:
+                    <ul>
+                        <li>
+                            <a href={'https://gameofthronesquotes.xyz/'}>
+                                https://gameofthronesquotes.xyz/
+                            </a>
+                        </li>
+                        <li>
+                            <a href={'https://thronesapi.com/swagger/index.html?urls.primaryName=Game of Thrones API v2'}>
+                                https://thronesapi.com/swagger/index.html?urls.primaryName=Game of Thrones API v2
+                            </a>
+                        </li>
+                        <li>
+                            <a href={'https://anapioficeandfire.com/'}>
+                                https://anapioficeandfire.com/
+                            </a>
+                        </li>
+                    </ul>
+                    Il sito ci guida all’interno del mondo del Trono di Spade creato dallo scrittore George R.R. Martin e successivamente reso celebre dalla serie tv omonima ideata da David Benioff e D. B. Weiss.
+                    A partire dalle famiglie più importanti possiamo scoprire i personaggi che popolano questo mondo: quando sono nati, i loro titoli, i loro soprannomi, quali sono le relazioni tra di loro, in quale stagione della serie tv possiamo trovarli e quali attori li hanno interpretati.
+                    Questo progetto è stato creato da Matilde Cotti per l’insegnamento di Applicazioni Web: Progettazione e Sviluppo del corso di Teoria e Tecnologia della Comunicazione.
+                </h2>
             </div>
         </div>
         )
