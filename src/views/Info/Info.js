7f20cabca99770dd454b8f6c17779c7108cab554
Mati <m.cotti1@campus.unimib.it>
2024-02-18 17:14:02 +0100

    info

--- src/views/Info/Info.js
+++ src/views/Info/Info.js
@@ -23,9 +23,9 @@ function Info () {
                             </a>
                         </li>
                     </ul>
-                    Il sito ci guida all’interno del mondo del Trono di Spade creato dallo scrittore George R.R. Martin e successivamente reso celebre dalla serie tv omonima ideata da David Benioff e D. B. Weiss.
-                    A partire dalle famiglie più importanti possiamo scoprire i personaggi che popolano questo mondo: quando sono nati, i loro titoli, i loro soprannomi, quali sono le relazioni tra di loro, in quale stagione della serie tv possiamo trovarli e quali attori li hanno interpretati.
-                    Questo progetto è stato creato da Matilde Cotti per l’insegnamento di Applicazioni Web: Progettazione e Sviluppo del corso di Teoria e Tecnologia della Comunicazione.
+                    Il sito ci guida all’interno del mondo del <b>Trono di Spade</b> creato dallo scrittore George R.R. Martin e successivamente reso celebre dalla serie tv omonima ideata da David Benioff e D. B. Weiss.
+                    A partire dalle famiglie più importanti possiamo scoprire i <b>personaggi che popolano questo mondo</b>: quando sono nati, i loro titoli, i loro soprannomi, quali sono le relazioni tra di loro, in quale stagione della serie tv possiamo trovarli e quali attori li hanno interpretati.
+                    <br/>Questo progetto è stato creato da Matilde Cotti per l’insegnamento di Applicazioni Web: Progettazione e Sviluppo del corso di Teoria e Tecnologia della Comunicazione.
                 </h2>
             </div>
         </div>
